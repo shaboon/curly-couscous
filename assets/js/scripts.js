c45a62dde3243c376b14366b10db60e75379f3a9
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-03-26 17:18:34 -0400

    Fixed API data draw, time remaining and local

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,91 +1,69 @@
 $(document).ready(function () {
-  var apiKey = "0578539c9ba7ff6fc8e3f6f54cac232c";
-  var searchEl = document.getElementsByClassName("cityvalue");
+  var apiKey = "88b98ce88c750083cc3efc235475dd54";
+  var searchEl = document.querySelector(".cityvalue");
   var citySearch = $(".inputcity");
-
-  function getWeather(lat, lon) {
-    console.log("getWeather Started");
-    fetch(
-      "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" +
-        lat +
-        "&lon=" +
-        lon +
-        "&appid=" +
-        apiKey
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        // Current Weather
-        var today = document.querySelectorAll(".today");
-        for (let i = 0; i < 1; i++) {
-          currentDate = data.list[i].dt;
-          var time = document.getElementsByClassName("date");
-          var image = document.getElementsByClassName("pic");
-          var temp = document.getElementsByClassName("temp");
-          var wind = document.getElementsByClassName("wind");
-          var hum = document.getElementsByClassName("humd");
-          time.innerHTML = dayjs.unix(currentDate).format("MMM/DD/YYYY");
-          temp.innerHTML = "Temp: " + data.list[i].main.temp_min + "°F";
-          // image.setAttribute(
-          //   "src",
-          //   "https://openweathermap.org/img/wn/" +
-          //     data.list[i].weather[0].icon +
-          //     "@2x.png"
-          // );
-          // image.setAttribute("alt", list[i].weather[0].description);
-          wind.innerHTML = "Wind: " + data.list[i].wind.speed + "MPH";
-          hum.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
-        }
-        // 5 DAY WEATHER DATA
-        console.log(data);
-        var weather = document.querySelectorAll(".wcard");
-        for (let i = 0; i < 1; i++) {
-          currentDate = data.list[i].dt;
-          var time = document.getElementsByClassName("date");
-          var image = document.getElementsByClassName("pic");
-          var temp = document.getElementsByClassName("temp");
-          var wind = document.getElementsByClassName("wind");
-          var hum = document.getElementsByClassName("humd");
-          time.innerHTML = dayjs.unix(currentDate).format("MMM/DD/YYYY");
-          temp.innerHTML = "Temp: " + data.list[i].main.temp_min + "°F";
-          // image.setAttribute(
-          //   "src",
-          //   "https://openweathermap.org/img/wn/" +
-          //     data.list[i].weather[0].icon +
-          //     "@2x.png"
-          // );
-          // image.setAttribute("alt", list[i].weather[0].description);
-          wind.innerHTML = "Wind: " + data.list[i].wind.speed + "MPH";
-          hum.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
-        }
-        console.log(data);
-        console.log(weather);
-      });
-  }
 
   function init() {
     console.log("init Started");
-    var city = "greensboro";
 
     // calls API and sets required parameters such as city and key
     fetch(
-      "https://api.openweathermap.org/geo/1.0/direct?q=" +
-        city +
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        searchEl.value +
         "&limit=" +
         1 +
         "&appid=" +
         apiKey
     )
       .then(function (response) {
+        console.log(response);
         return response.json();
       })
       // from data, lat and lon is pulled, then function is given variables
       .then(function (data) {
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        getWeather(lat, lon);
+        // Current Weather
+        var today = document.querySelectorAll(".today");
+        for (let i = 0; i < today.length; i++) {
+          var nowDate = data.list[i].dt;
+          var time = document.getElementsByClassName("date");
+          var image = document.getElementsByClassName("pic");
+          var temp = document.getElementsByClassName("temp");
+          var wind = document.getElementsByClassName("wind");
+          var hum = document.getElementsByClassName("humd");
+          time.innerHTML = dayjs.unix(nowDate).format("MMM/DD/YYYY");
+          temp.innerHTML = "Temp: " + data.list[i].main.temp_min + "°F";
+          // image.setAttribute(
+          //   "src",
+          //   "https://openweathermap.org/img/wn/" +
+          //     data.list[i].weather.icon
+          // );
+          // image.setAttribute("alt", data.list[i].weather.description);
+          wind.innerHTML = "Wind: " + data.list[i].wind.speed + " MPH";
+          hum.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+        }
+        // 5 DAY WEATHER DATA
+        console.log(data);
+        var weather = document.querySelectorAll(".wcard");
+        for (let i = 0; i < weather.length; i++) {
+          var currentDate = data.list[i].dt;
+          var time = document.getElementsByClassName("date");
+          var image = document.getElementsByClassName("pic");
+          var temp = document.getElementsByClassName("temp");
+          var wind = document.getElementsByClassName("wind");
+          var hum = document.getElementsByClassName("humd");
+          // time[i].innerHTML = dayjs.unix(currentDate).format("MMM/DD/YYYY");
+          temp[i].innerHTML = "Temp: " + data.list[i].main.temp_min + "°F";
+          // image.setAttribute(
+          //   "src",
+          //   "https://openweathermap.org/img/wn/" +
+          //     data.list[i].weather.icon
+          // );
+          // image.setAttribute("alt", data.list[i].weather.description);
+          wind[i].innerHTML = "Wind: " + data.list[i].wind.speed + " MPH";
+          hum[i].innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+        }
+        console.log(data);
+        console.log(weather);
       });
   }
 
