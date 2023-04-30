30db40237c3a06da1b9f7d886308e71483fca09e
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-04-30 12:10:35 -0400

    Weather JS completed, button and search history fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,15 +3,26 @@ $(document).ready(function () {
   var searchEl = document.querySelector(".cityvalue");
   var citySearch = $(".inputcity");
   var listEl = $(".searches");
-  var history = localStorage.getItem("searches");
+  var historyEl = $(".history");
+  var weatherTarget = $(".weather");
+  var history = JSON.parse(localStorage.getItem("searches")) || [];
 
   function init() {
-    console.log("init Started");
+    console.log(history);
+    let i = history.length - 1;
+    console.log(i);
+    console.log(history[i]);
+    let location = history[i];
+    searchCity(location);
+  }
+
+  function searchCity(location) {
+    console.log("searchCity Started");
 
     // calls API and sets required parameters such as city and key
     fetch(
       "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" +
-        searchEl.value +
+        location +
         "&limit=" +
         1 +
         "&appid=" +
@@ -23,6 +34,7 @@ $(document).ready(function () {
       })
       // from data, lat and lon is pulled, then function is given variables
       .then(function (data) {
+        weatherTarget.textContent = "The Current Weather at" + location + "is:";
         var todayDate = data.list[0].dt;
         console.log(todayDate);
         console.log(0);
@@ -34,7 +46,7 @@ $(document).ready(function () {
         var hum = document.getElementsByClassName("humd");
         time[0].textContent =
           "Currently at " +
-          searchEl.value +
+          location +
           " on " +
           dayjs.unix(todayDate).format("MMM/DD/YYYY");
         temp[0].innerHTML = "Temp: " + data.list[0].main.temp_min + "°F";
@@ -51,9 +63,9 @@ $(document).ready(function () {
         console.log(data);
         var weather = document.querySelectorAll(".wcard");
         for (let i = 0; i < weather.length; i++) {
-          var currentDate = data.list[i * 8 + 4].dt;
+          var currentDate = data.list[i * 8 + 6].dt;
           console.log(currentDate);
-          console.log(i * 8 + 4);
+          console.log(i * 8 + 6);
           console.log(dayjs.unix(currentDate).format("MMM/DD/YYYY"));
           var blocktime = document.getElementsByClassName("blocktime");
           var image = document.getElementsByClassName("status");
@@ -64,7 +76,7 @@ $(document).ready(function () {
             .unix(currentDate)
             .format("MMM/DD/YYYY");
           blocktemp[i].innerHTML =
-            "Temp: " + data.list[i * 8 + 4].main.temp_min + "°F";
+            "Temp: " + data.list[i * 8 + 6].main.temp_min + "°F";
           image[i].setAttribute(
             "src",
             "https://openweathermap.org/img/wn/" +
@@ -73,9 +85,9 @@ $(document).ready(function () {
           );
           image[i].setAttribute("alt", data.list[i].weather[0].description);
           blockwind[i].innerHTML =
-            "Wind: " + data.list[i * 8 + 4].wind.speed + " MPH";
+            "Wind: " + data.list[i * 8 + 6].wind.speed + " MPH";
           blockhum[i].innerHTML =
-            "Humidity: " + data.list[i * 8 + 4].main.humidity + "%";
+            "Humidity: " + data.list[i * 8 + 6].main.humidity + "%";
         }
         console.log(data);
         console.log(weather);
@@ -90,27 +102,39 @@ $(document).ready(function () {
     console.log("line 45");
 
     // Stringifies object to JSON string for easy localStorage call opposed to multiple values having to be called
-    // history.push(searchEl);
-    // renderSearches();
-    // console.log("line 71");
+    history.push(searchEl.value);
+    localStorage.setItem("searches", JSON.stringify(history));
+    historyEl.remove();
+    renderSearches();
+    console.log("line 71");
 
-    init();
+    searchCity(searchEl.value);
   });
 
   // localize code
-  // function renderSearches() {
-  //   history.innerHTML = "";
-  //   for (let i = 0; i < searchHistory.length; i++) {
-  //     const historyItem = document.createElement("input");
-  //     historyItem.setAttribute("type", "text");
-  //     historyItem.setAttribute("readonly", true);
-  //     historyItem.setAttribute("class", "form-control d-block bg-white");
-  //     historyItem.setAttribute("value", searchHistory[i]);
-  //     historyItem.addEventListener("click", function () {
-  //       getWeather(historyItem.value);
-  //     });
-  //     historyEl.append(historyItem);
-  //   }
-  // }
-  // renderSearches();
+  function renderSearches() {
+    listEl.empty();
+    for (let i = 0; i < history.length; i++) {
+      console.log(history.length);
+      const historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute(
+        "class",
+        "history col-12 text-center form-control d-block bg-dark text-white"
+      );
+      historyItem.setAttribute("value", history[i]);
+      historyItem.addEventListener("click", function () {
+        searchCity(historyItem.value);
+      });
+      listEl.append(historyItem);
+      console.log("Line 114");
+    }
+  }
+  renderSearches();
+  if (history > 0) {
+  }
+  init();
+
+  console.log("Line 118");
 });
